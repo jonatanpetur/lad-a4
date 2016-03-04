be76d2bb619e208ef05aca90c88bc47c45ba8313
Jonatan <jonatan.petur@gmail.com>
2016-03-04 09:38:02 +0100

    Task 1 completed ?

--- src/lad_a4/CountingFilterImpl.java
+++ src/lad_a4/CountingFilterImpl.java
@@ -1,40 +1,36 @@
 package lad_a4;
 
 public class CountingFilterImpl implements CountingFilter {
-   
+
+    private int[] filter;
+
     /** Initiates a counting filter, the capacity indicates how much memory can be used
      * (memory usage must be O(capacity)) */
     public CountingFilterImpl(int capacity){
-        // TODO Auto-generated method stub
+        filter = new int[capacity];
     }
-    
 
     public void add(int e) {
-        // TODO Auto-generated method stub
+        filter[e % filter.length]++;
     }
 
     public void add(Object e) {
-        // TODO Auto-generated method stub
-        // Hint: Call the other add method
+        this.add(e.hashCode());
     }
 
     public int count(int e) {
-        // TODO Auto-generated method stub
-        return 0;
+        return filter[e % filter.length];
     }
 
     public int count(Object e) {
-        // TODO Auto-generated method stub
-        // Hint: Call the other count method
-        return 0;
+        return this.count(e.hashCode());
     }
     
     public void remove(int e) {
-        // TODO Auto-generated method stub
+        filter[e % filter.length]--;
     }
 
     public void remove(Object e) {
-        // TODO Auto-generated method stub
-        // Hint: Call the other remove method
+        this.remove(e.hashCode());
     }
 }
