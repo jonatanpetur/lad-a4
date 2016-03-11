15231c9097b6899d9aaa3691f256ea8fb050e7a2
Jonatan <jonatan.petur@gmail.com>
2016-03-11 15:22:25 +0100

    Changed Kafka out for Dickens and added a hashmap to Wordcount

--- src/lad_a4/WordCount.java
+++ src/lad_a4/WordCount.java
@@ -1,28 +1,32 @@
 package lad_a4;
 
 import java.io.FileInputStream;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class WordCount {
     // Usage: Either provide path and capacity as args or hardcode into the source
     public static void main(String[] args) {
-        int defaultCapacity = 100000;
+        int defaultCapacity = 20000;
         String defaultPath = "file.txt";
         if (args.length<2)
             countWords(defaultPath,defaultCapacity);
         else
             countWords(args[0],Integer.parseInt(args[1]));
     }
-    
+
 
     
     static void countWords(String path, int capacity){
+        HashMap<Integer, String> hm = new HashMap<>();
         CountingFilter cf = new CountingFilterImpl(capacity);
         try{
             Scanner sc = new Scanner(new FileInputStream(path)).useDelimiter("[ ,!?.:;\n]+");
             int n = 0;
             while (sc.hasNext()){
-                cf.add(sc.next().toLowerCase());
+                String word = sc.next().toLowerCase();
+                cf.add(word);
+                hm.put(word.hashCode(), word);
                 n++;
             }
             sc.close();
